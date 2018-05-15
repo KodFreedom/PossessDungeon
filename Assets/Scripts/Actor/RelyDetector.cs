99ea3ae3d43ab0b1af32eee857d522706e3613c9
KodFreedom <kodfreedom@gmail.com>
2018-05-15 15:12:31 +0900

    魂とモンスターの当たり判定を消し、乗り移る検索を仕様通りに修正

--- Assets/Scripts/Actor/RelyDetector.cs
+++ Assets/Scripts/Actor/RelyDetector.cs
@@ -9,6 +9,8 @@ namespace ProjectBaka
         private GameObject target_ = null;
         public GameObject Target { get { return target_; } }
 
+        int actor_layer_ = 0;
+
         // Use this for initialization
         void Start()
         {
@@ -18,23 +20,42 @@ namespace ProjectBaka
             collider.isTrigger = true;
             collider.center = new Vector3(0.0f, 0.0f, 5.0f);
             collider.size = new Vector3(2.0f, 5.0f, 10.0f);
+            actor_layer_ = LayerMask.NameToLayer("Actor");
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerStay(Collider other)
         {
-            if (other.gameObject.layer == LayerMask.NameToLayer("Actor"))
+            if (other.gameObject.layer != actor_layer_
+                || target_ == other.gameObject) return;
+
+            if (target_ == null)
             {
                 target_ = other.gameObject;
+            }
+            else
+            {// 距離を計算し、近いの方をターゲットにする
+                target_ = GetClosestTarget(target_, other.gameObject);
             }
         }
 
         private void OnTriggerExit(Collider other)
         {
-            if (other.gameObject.layer == LayerMask.NameToLayer("Actor")
+            if (other.gameObject.layer == actor_layer_
                 && other.gameObject == target_)
             {
                 target_ = null;
             }
+        }
+
+        private GameObject GetClosestTarget(GameObject target_a, GameObject target_b)
+        {
+            Vector3 parent_position = transform.parent.position;
+            if(Vector3.SqrMagnitude(parent_position - target_a.transform.position)
+                <= Vector3.SqrMagnitude(parent_position - target_b.transform.position))
+            {
+                return target_a;
+            }
+            return target_b;
         }
     }
 }
