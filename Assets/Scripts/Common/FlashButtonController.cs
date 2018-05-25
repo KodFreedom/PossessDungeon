882a6c6e807f50a6701c3cade2f29db795b8e67d
kodfreedom <kodfreedom@gmail.com>
2018-05-25 11:24:14 +0900

    fadeシステムとsceneマネージャ

---
+++ Assets/Scripts/Common/FlashButtonController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+namespace ProjectBaka
+{
+    public class FlashButtonController : MonoBehaviour
+    {
+        /////////////////////////////////////////////////////////////////////////
+        // 定数宣言
+        /////////////////////////////////////////////////////////////////////////
+        [SerializeField] float kNormalFlashingTime = 0.5f;
+        [SerializeField] float kPushedFlashingTime = 0.1f;
+        [SerializeField] float kWaitForChangeScene = 0.5f;
+        [SerializeField] string kNextSceneName = string.Empty;
+
+        /////////////////////////////////////////////////////////////////////////
+        // 変数宣言
+        /////////////////////////////////////////////////////////////////////////
+        private float flashing_time_ = 0.0f;
+        private float time_counter_ = 0.0f;
+        private float time_counter_for_change_scene_ = 0.0f;
+        private int   sign_ = 1;
+        private Image image_ = null;
+
+        /////////////////////////////////////////////////////////////////////////
+        // Private関数
+        /////////////////////////////////////////////////////////////////////////
+        // 初期化処理
+        private void Start()
+        {
+            // 変数初期化
+            flashing_time_ = kNormalFlashingTime;
+            time_counter_ = 0.0f;
+            time_counter_for_change_scene_ = 0.0f;
+            sign_ = 1;
+
+            // Image Componentを探す
+            image_ = GetComponentInChildren<Image>();
+            Assert.IsNotNull(image_, "FlashButtonController: Image Componentが見つからない！");
+
+            // ColorのAlpha値を0にする
+            image_.color = new Color(1f, 1f, 1f, 0f);
+        }
+
+        // 更新処理
+        private void Update()
+        {
+            // Alpha値更新
+            float alpha = time_counter_ / flashing_time_;
+            image_.color = new Color(1f, 1f, 1f, alpha);
+
+            // 点滅カウント
+            time_counter_ += sign_ * Time.deltaTime;
+
+            // カウンターが0かフラッシング時間になったら符号を転換する
+            if (time_counter_ >= flashing_time_
+                || time_counter_ <= 0f)
+            {
+                sign_ *= -1;
+            }
+
+            // 表示までの遅延演出
+            if (time_counter_for_change_scene_ > 0f)
+            {
+                time_counter_for_change_scene_ -= Time.deltaTime;
+                if (time_counter_for_change_scene_ <= 0f)
+                {
+                    time_counter_for_change_scene_ = -1f;
+                    SceneManager.Instance.ChangeScene(kNextSceneName);   
+                }
+                return;
+            }
+
+            // ボタン押す判定
+            if (Input.GetButtonDown("Submit"))
+            {
+                // 点滅間隔更新
+                flashing_time_ = kPushedFlashingTime;
+                time_counter_ = alpha * flashing_time_;
+
+                time_counter_for_change_scene_ = kWaitForChangeScene;
+
+                // se再生
+                //SoundPlayer.Instance.PlayOneShot(SoundSE.Select);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Common/SceneManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace ProjectBaka
+{
+    public class SceneManager : MonoBehaviour
+    {
+        /////////////////////////////////////////////////////////////////////////
+        // シングルトーンインスタンス
+        /////////////////////////////////////////////////////////////////////////
+        private static SceneManager instance_ = null;
+        public static SceneManager Instance { get { return instance_; } }
+
+        /////////////////////////////////////////////////////////////////////////
+        // 定数宣言
+        /////////////////////////////////////////////////////////////////////////
+        private const float kWaitTime = 0.5f;
+
+        /////////////////////////////////////////////////////////////////////////
+        // 変数宣言
+        /////////////////////////////////////////////////////////////////////////
+        private FadeController fade_controller_ = null;
+        private string next_scene_name_ = string.Empty;
+        private float time_counter_ = 0f;
+
+        /////////////////////////////////////////////////////////////////////////
+        // Public関数
+        /////////////////////////////////////////////////////////////////////////
+        /// <summary> Fadeでシーンを切り替える </summary>
+        public void ChangeScene(string next_scene_name)
+        {
+            if (next_scene_name_.Length != 0) return;
+            next_scene_name_ = next_scene_name;
+            fade_controller_.StartFadeOut();
+        }
+
+        /// <summary> シーンの切り替えの開始（FadeControllerが呼ぶ） </summary>
+        public void StartChange()
+        {
+            time_counter_ = kWaitTime;
+        }
+
+        /////////////////////////////////////////////////////////////////////////
+        // Private関数
+        /////////////////////////////////////////////////////////////////////////
+        // 初期化処理
+        private void Start()
+        {
+            // インスタンスが生成されてるかどうかをチェックする
+            if (null == instance_)
+            {
+                // ないなら自分を渡す
+                instance_ = this;
+            }
+            else
+            {
+                // すでにあるなら自分を破棄する
+                DestroyImmediate(gameObject);
+                return;
+            }
+
+            // FadeControllerを探す
+            fade_controller_ = gameObject.GetComponentInChildren<FadeController>();
+            Assert.IsNotNull(fade_controller_, "SceneManager: FadeControllerが見つからない！");
+
+            // シーン遷移の時破棄しない
+            DontDestroyOnLoad(gameObject);
+
+            // 最初のBGMを再生
+            //SoundPlayer.Instance.PlayBGM(SoundBGM.Title);
+        }
+
+        // 更新処理
+        private void Update()
+        {
+            if (0 == next_scene_name_.Length) return;
+            if (time_counter_ > 0f)
+            {
+                time_counter_ -= Time.unscaledDeltaTime;
+                if (time_counter_ <= 0f)
+                {
+                    // Scene遷移
+                    ChangeBgm(next_scene_name_);
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(next_scene_name_);
+                    next_scene_name_ = string.Empty;
+                    Time.timeScale = 1f;
+                    fade_controller_.StartFadeIn();
+                }
+            }
+        }
+
+        // BGMの変更
+        private void ChangeBgm(string next_scene_name_)
+        {
+            if (next_scene_name_ == "Title")
+            {
+                //SoundPlayer.Instance.PlayBGM(SoundBGM.Title);
+                return;
+            }
+            if (next_scene_name_ == "Game")
+            {
+                //SoundPlayer.Instance.PlayBGM(SoundBGM.Game);
+                return;
+            }
+        }
+    }
+}
