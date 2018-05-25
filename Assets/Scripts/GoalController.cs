55012fe5189cfc69681fc5daaf498993af6b404c
kodfreedom <kodfreedom@gmail.com>
2018-05-25 10:52:48 +0900

    goalのプレハブ追加

---
+++ Assets/Scripts/GoalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectBaka
+{
+    public class GoalController : MonoBehaviour
+    {
+        private int actor_layer_ = 0;
+        private int soul_layer_ = 0;
+
+        // Use this for initialization
+        private void Start()
+        {
+            actor_layer_ = LayerMask.NameToLayer("Actor");
+            soul_layer_ = LayerMask.NameToLayer("Soul");
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.layer != actor_layer_
+                && other.gameObject.layer != soul_layer_)
+                return;
+
+            var actor_controller = other.gameObject.GetComponent<ActorController>();
+            if (actor_controller.GetBrainType() != ActorController.BrainType.kPlayer) return;
+
+            // TODO: clear
+        }
+    }
+}
