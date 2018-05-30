9254e1231bfa8f8ddd311a2f26ac55324b41a9ea
THS50593\iijima.hayato <B19960606>
2018-05-30 12:10:11 +0900

    ステージ選択画面の追加(確認でステージ３つのシーンも追加)

---
+++ Assets/Scripts/StageSelect/CursorMove.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorMove : MonoBehaviour {
+
+    public RectTransform moveAfterRectPos;
+
+    public int posIdx = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveAfterRectPos = GetComponent<RectTransform>();
+
+        // カーソルの右移動
+        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            && posIdx < 2)
+        {
+            posIdx++;
+        }
+
+        // カーソルの左移動
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            && 0 < posIdx)
+        {
+            posIdx--;
+        }
+
+        // RectTransformの座標変更
+        switch(posIdx)
+        {
+            case 0:
+                moveAfterRectPos.anchoredPosition = new Vector2(-150.0f, 30.0f);
+                break;
+
+            case 1:
+                moveAfterRectPos.anchoredPosition = new Vector2(0.0f, 30.0f);
+                break;
+
+            case 2:
+                moveAfterRectPos.anchoredPosition = new Vector2(150.0f, 30.0f);
+                break;
+        }
+        
+
+
+        // Enter押下時に、インデックス番号からどのステージを選んでいるのか取得
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            switch (posIdx)
+            {
+                // Stage1シーンに遷移
+                case 0:
+                    SceneManager.LoadScene("Stage1");
+                    break;
+
+                // Stage2シーンに遷移
+                case 1:
+                    SceneManager.LoadScene("Stage2");
+                    break;
+
+                // Stage3シーンに遷移
+                case 2:
+                    SceneManager.LoadScene("Stage3");
+                    break;
+            }
+        }
+    }
+}
