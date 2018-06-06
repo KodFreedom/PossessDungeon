3599e9789ddf5c93547a72b483d131111edc6949
kodfreedom <kodfreedom@gmail.com>
2018-06-06 15:45:10 +0900

    Revert "Revert "パトロールポイントの仕様更新""
    This reverts commit 10b1cac389bdd235360baf92a6f1614a95a7d5a9.

--- Assets/Scripts/Game/Actor/PatrolPoints.cs
+++ Assets/Scripts/Game/Actor/PatrolPoints.cs
@@ -6,7 +6,22 @@ namespace ProjectBaka
 {
     public class PatrolPoints : MonoBehaviour
     {
-        [SerializeField] Transform[] points_;
-        public Transform[] Points { get { return points_; } }
+        [SerializeField] Transform[] local_points_;
+        private Vector3[] points_ = new Vector3[3];
+        public Vector3[] Points { get { return points_; } }
+
+        public void RecalculatePoints()
+        {
+            for(int iterator = 0; iterator < local_points_.Length; ++iterator)
+            {
+                points_[iterator] = local_points_[iterator].localPosition + transform.position;
+            }
+        }
+
+        private void Start()
+        {
+            points_ = new Vector3[local_points_.Length];
+            RecalculatePoints();
+        }
     }
 }
