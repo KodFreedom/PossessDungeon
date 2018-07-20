e0ff3ab248939e78f5bd1116efb103ea30f35c3a
将輝 小川 <jrp6exu3f@outlook.jp>
2018-07-20 10:17:56 +0900

    Tutorialに操作方法UIを追加

---
+++ Assets/Scripts/Title/DestroyUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Golem;
+
+    [SerializeField]
+    private bool UIFlag;
+
+    [SerializeField]
+    private GameObject Collibox;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UIFlag == false)
+        {
+            if (Golem.tag == "Player")
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if (UIFlag == true)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                if (Golem.tag == "Npc")
+                {
+                    Destroy(Collibox);
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Title/SpaceUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SpaceUIObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SpaceUIObj.GetComponent<SpriteRenderer>().enabled = true;
+            Destroy(this);
+        }
+    }
+}
